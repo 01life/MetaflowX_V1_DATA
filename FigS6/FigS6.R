d9810f1ea51c38e6a921b21c24cbaa044d294770
LiangLifeng <llfgg2016@gmail.com>
2024-08-23 14:49:44 +0800

    Fig2ab + FigS7

--- FigS6/FigS6.R
+++ FigS6/FigS6.R
@@ -21,20 +21,25 @@ color_var = c("#7570b3", "#a6761d", "#1b9e77","#1f78b4", "#e7298a", "#BDC367")
 biner_list <- c("MetaBAT2","CONCOCT","SemiBin2","MaxBin2","MetaBinner","COMEBin","binny")
 
 
-profile <- read.table("./6CAMI.dataset.7binner.running.info.rename.txt",header=T, sep = "\t",check.name = F)
+pdf("./FigS6.legend.pdf",width = 3,height = 3)
+plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
+legend("topleft", legend =group_list, pch=16, pt.cex=0.7, cex=0.7, bty='n',
+       col = color_var) 
+mtext("CAMI dataset", at=0.05, cex=0.7)
+dev.off()
+
+
+profile <- read.table("./6CAMI.dataset.7binner.running.info.txt",header=T, sep = "\t",check.name = F)
+
+profile$ID <- profile$sampleID
 
 map <- read.table("../group.txt",header=T, sep = "\t", check.name = F,row.names=1)
 map$ID <- rownames(map)
 
 map <- subset(map,Group %in% group_list)
-
-
 needID <- intersect(map$ID, profile$ID)
 map <- subset(map,ID %in% needID)
-
 profile <- subset(profile,ID %in% needID)
-
-  
 plot <- merge(profile,map,by="ID",all=TRUE)
 
 plot$task <- factor(plot$task,levels=biner_list)
@@ -42,26 +47,26 @@ plot$Group <- factor(plot$Group,levels=group_list)
 
 cpu <- ggboxplot(plot, x = "task", y = "cpu",alpha=0.7,fill = "Group",outlier.size = 0.7,
                   ggtheme = theme_bw() +
-                    theme(axis.text.x = element_text(color="black",size=10,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
-                          axis.text.y = element_text(color="black",size=10,face="bold"),
-                          axis.title.y=element_text(color="black",size=16,face="bold"),
+                    theme(axis.text.x = element_text(color="black",size=8,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
+                          axis.text.y = element_text(color="black",size=8,face="bold"),
+                          axis.title.y=element_text(color="black",size=8,face="bold"),
                           axis.line = element_line(color="black"),
                           axis.ticks = element_line(color="black"),
                           strip.text.x = element_text(size = 8, colour = "black", face="bold"), 
                           strip.background  = element_blank(),
                           legend.position = "none",
-                          legend.text = element_text(size = 10, colour = "black"),
-                          legend.title = element_text(size = 10),
+                          legend.text = element_text(size=8, colour = "black"),
+                          legend.title = element_text(size=8),
                           legend.key.width = unit(0.3, 'cm'),
                           legend.key.size = unit(0, 'lines'),
                           panel.grid = element_blank(),
                           panel.background = element_blank()
                           ),
-                  legend = "none",title = "",xlab = '', ylab = 'CPU',width = 0.7)+
+                  legend = "none",title = "",xlab = '', ylab = 'CPU Utilization (%)',width = 0.7)+
   scale_y_continuous(expand = expansion(mult = c(0.01, 0.1)))+
   scale_fill_manual(values=color_var,labels=group_list,limits = group_list, breaks = group_list)
 
-pdf("./cpu.pdf",width = 8,height = 4)
+pdf("FigS6.a.pdf",width = 8,height = 3)
 cpu
 dev.off()
 
@@ -69,16 +74,16 @@ dev.off()
 realtime <- ggboxplot(plot, x = "task", y = "realtime_M",alpha=0.7,fill = "Group",outlier.size = 0.7,
                  #palette = "jco",
                  ggtheme = theme_bw() +
-                   theme(axis.text.x = element_text(color="black",size=10,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
-                         axis.text.y = element_text(color="black",size=10,face="bold"),
-                         axis.title.y=element_text(color="black",size=16,face="bold"),
+                   theme(axis.text.x = element_text(color="black",size=8,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
+                         axis.text.y = element_text(color="black",size=8,face="bold"),
+                         axis.title.y=element_text(color="black",size=8,face="bold"),
                          axis.line = element_line(color="black"),
                          axis.ticks = element_line(color="black"),
                          strip.text.x = element_text(size = 8, colour = "black", face="bold"), 
                          strip.background  = element_blank(),
                          legend.position = "none",
-                         legend.text = element_text(size = 10, colour = "black"),
-                         legend.title = element_text(size = 10),
+                         legend.text = element_text(size=8, colour = "black"),
+                         legend.title = element_text(size=8),
                          legend.key.width = unit(0.3, 'cm'),
                          legend.key.size = unit(0, 'lines'),
                          panel.grid = element_blank(),
@@ -87,7 +92,7 @@ realtime <- ggboxplot(plot, x = "task", y = "realtime_M",alpha=0.7,fill = "Group
   scale_y_continuous(expand = expansion(mult = c(0.01, 0.1)))+
   scale_fill_manual(values=color_var,labels=group_list,limits = group_list, breaks = group_list)
 
-pdf(".//realtime_M.pdf",width = 8,height = 4)
+pdf("FigS6.b.pdf",width = 8,height = 3)
 realtime
 dev.off()
 
@@ -98,16 +103,16 @@ dev.off()
 peak <- ggboxplot(plot, y = "peak_rss_G", x = "task",alpha=0.7,fill = "Group",outlier.size = 0.7,
                       #palette = "jco",
                       ggtheme = theme_bw() +
-                        theme(axis.text.x = element_text(color="black",size=10,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
-                              axis.text.y = element_text(color="black",size=10,face="bold"),
-                              axis.title.y=element_text(color="black",size=16,face="bold"),
+                        theme(axis.text.x = element_text(color="black",size=8,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
+                              axis.text.y = element_text(color="black",size=8,face="bold"),
+                              axis.title.y=element_text(color="black",size=8,face="bold"),
                               axis.line = element_line(color="black"),
                               axis.ticks = element_line(color="black"),
                               strip.text.x = element_text(size = 8, colour = "black", face="bold"), 
                               strip.background  = element_blank(),
                               legend.position = "none",
-                              legend.text = element_text(size = 10, colour = "black"),
-                              legend.title = element_text(size = 10),
+                              legend.text = element_text(size=8, colour = "black"),
+                              legend.title = element_text(size=8),
                               legend.key.width = unit(0.3, 'cm'),
                               legend.key.size = unit(0, 'lines'),
                               panel.grid = element_blank(),
@@ -116,35 +121,9 @@ peak <- ggboxplot(plot, y = "peak_rss_G", x = "task",alpha=0.7,fill = "Group",ou
   scale_y_continuous(expand = expansion(mult = c(0.01, 0.1)))+
   scale_fill_manual(values=color_var,labels=group_list,limits = group_list, breaks = group_list)
 
-pdf("./peak_rss_G.png",width = 8,height = 4)
+pdf("FigS6.c.pdf",width = 8,height = 3)
 peak
 dev.off()
 
-
-plot$logPeak <- log(plot$peak_rss_G)
-peak <- ggboxplot(plot, y = "logPeak", x = "task",alpha=0.7,fill = "Group",outlier.size = 0.7,
-                  #palette = "jco",
-                  ggtheme = theme_bw() +
-                    theme(axis.text.x = element_text(color="black",size=10,angle=0,hjust= 0.5 ,vjust = 0 ,face="bold"),
-                          axis.text.y = element_text(color="black",size=10,face="bold"),
-                          axis.title.y=element_text(color="black",size=16,face="bold"),
-                          axis.line = element_line(color="black"),
-                          axis.ticks = element_line(color="black"),
-                          strip.text.x = element_text(size = 8, colour = "black", face="bold"), 
-                          strip.background  = element_blank(),
-                          legend.position = "none",
-                          legend.text = element_text(size = 10, colour = "black"),
-                          legend.title = element_text(size = 10),
-                          legend.key.width = unit(0.3, 'cm'),
-                          legend.key.size = unit(0, 'lines'),
-                          panel.grid = element_blank(),
-                          panel.background = element_blank()),
-                  legend ="none" ,title = "",xlab = '', ylab = 'Peak Resident Set Size (G)',width = 0.7)+
-  scale_y_continuous(expand = expansion(mult = c(0.01, 0.1)))+
-  scale_fill_manual(values=color_var,labels=group_list,limits = group_list, breaks = group_list)
-
-pdf("./logPeak.pdf",width = 8,height = 4)
-peak
-dev.off()
 
 
--- work.sh
+++ work.sh
@@ -1,3 +1,40 @@
+#Fig2
+    cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/paper/MetaflowX_V1_DATA/Fig2
+
+    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/01_binner_evaluation/amber/result/*/*_original_binner_AMBER/results.tsv |head -n 1 |while read a ;do head -n 1 $a ;done > p0
+    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/01_binner_evaluation/amber/result/*/*_original_binner_AMBER/results.tsv | while read a ;do tail -n +2 $a ;done  > p1
+    sed 's/Gold standard/GS/g' p1 |sed 's/concoct/CONCOCT/g' |sed 's/comebin/COMEBin/g' |sed 's/maxbin/MaxBin2/g' |sed 's/metabat/MetaBAT2/g' |sed 's/metabinner/MetaBinner/g' |sed 's/semibin2/SemiBin2/g'  > p2
+    cat p0 p2 > 6dataset_7binner_single_binner_amber.txt
+
+
+    #mutlti biner
+    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/bestbinner/Result/BestBinnerDetector/MetaflowX_binner_DASToolsCombine_Bining_Percentage.*xls |awk -F "/" '{print"tail -n +2 "$0 "  |sed \"s/$/\\t"$7"/g\" >"$7".percentage"}' |sh
+
+    cat *percentage |cut -f 2- > 6dataset_combine_7binner_Bining_Percentage.txt
+
+    sed  -i 1i"binner\tBinType\tavg_Prevalence\tdataset" 6dataset_combine_7binner_Bining_Percentage.txt
+
+    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/bestbinner/Result/BestBinnerDetector/MetaflowX_binner_DASToolsCombine_preSample_avgBinScore_counts.xls |awk -F "/" '{print"tail -n +2 "$0 "  |sed \"s/$/\\t"$7"/g\" >"$7".count"}' |sh
+
+    cat *count  |cut -f 2- > 6dataset_combine_7binner_preSample_avgBinScore_counts.txt
+    sed  -i 1i"binner\tBinScore\tavg_counts\tdataset" 6dataset_combine_7binner_preSample_avgBinScore_counts.txt
+
+
+
+
+    grep -wE "concoct_semibin2_metabat|metabat_concoct_semibin2|semibin2_concoct_metabat|semibin2_metabat_concoct" /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/bestbinner/Result/BestBinnerDetector/MetaflowX_binner_DASToolsCombine_Bining_Percentage.*xls |awk -F "/" '{print$7"\t"$NF}' |cut -f 1,3,4,5 > metabat_concoct_semibin2.Bining_Percentage.xls
+
+    grep -wE "concoct_metabat_maxbin_metabinner|concoct_semibin2_maxbin_metabinner|concoct_semibin2_metabat_maxbin|concoct_semibin2_metabat_metabinner|maxbin_concoct_metabinner_metabat|maxbin_concoct_semibin2_metabat|maxbin_concoct_semibin2_metabinner|maxbin_metabat_concoct_metabinner|maxbin_metabat_concoct_semibin2|maxbin_metabat_semibin2_metabinner|maxbin_metabinner_concoct_metabat|maxbin_metabinner_semibin2_concoct|maxbin_metabinner_semibin2_metabat|maxbin_semibin2_concoct_metabat|maxbin_semibin2_metabat_concoct|metabat_concoct_semibin2_metabinner|metabinner_concoct_semibin2_metabat|metabinner_maxbin_concoct_metabat|metabinner_maxbin_concoct_semibin2|metabinner_maxbin_metabat_concoct|metabinner_maxbin_semibin2_concoct|metabinner_maxbin_semibin2_metabat|metabinner_semibin2_concoct_metabat|metabinner_semibin2_metabat_concoct|semibin2_concoct_metabinner_metabat|semibin2_maxbin_concoct_metabat|semibin2_maxbin_concoct_metabinner|semibin2_maxbin_metabinner_metabat|semibin2_metabat_maxbin_metabinner" /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/bestbinner/Result/BestBinnerDetector/MetaflowX_binner_DASToolsCombine_Bining_Percentage.*xls |awk -F "/" '{print$7"\t"$NF}' |cut -f 1,3,4,5 > metabat_concoct_semibin2.Bining_Percentage.xls
+
+
+#FigS5
+    cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/paper/MetaflowX_V1_DATA/FigS5
+    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/*Result/02.Contig/*stat | while read a ;do tail -n +2 $a ;done > p1 
+    cat /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/*Result/02.Contig/*stat |head -n 1 > p0 
+    cat p0 p1 > 6CAMI.dataset.all.sample.contig.stat.txt 
+    rm -rf p0 p1 
+
+
 ##3 runing time of varies binner
 
     cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/airskin/Result/pipeline_info
@@ -25,20 +62,10 @@
     python /aimigene/lianglifeng/pipeline/01.metagenome/modules/MetaFlowX_Plotly_Report/computer.perfomance.transfer.py all.execution_trace.txt METABAT2:MAXBIN2:CONCOCT:METABINNER:SEMIBIN2:BINNY:COMEBIN strain.binnigTools.stat.txt
 
     #merge running info
-    cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/paper/MetaflowX_V1_DATA/Fig2
+    cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/paper/MetaflowX_V1_DATA/FigS6
     ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/Result/pipeline_info/*binnigTools.stat.txt |while read a ;do tail -n +2 $a ;done > p1 
     cat /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/Result/pipeline_info/*binnigTools.stat.txt |head -n 1 > p0 
     cat p0 p1 > 6CAMI.dataset.7binner.running.info.txt
     rm -rf p0 p1 
 
     le 6CAMI.dataset.7binner.running.info.txt | sed 's/BINNY/binny/g' |sed 's/COMEBIN/COMEBin/g' |sed 's/CONCOCT/CONCOCT/g' |sed 's/MAXBIN2/MaxBin2/g'  | sed 's/METABAT2/MetaBAT2/g' |sed 's/SEMIBIN2/SemiBin2/g' |sed 's/METABINNER/MetaBinner/g' > 6CAMI.dataset.7binner.running.info.rename.txt
-    
-
-
-
-#FigS5
-    cd /aimigene/lianglifeng/01.Project/09.MetaFlowX/paper/MetaflowX_V1_DATA/FigS5
-    ls /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/*Result/02.Contig/*stat | while read a ;do tail -n +2 $a ;done > p1 
-    cat /aimigene/lianglifeng/01.Project/09.MetaFlowX/06_multi_binner/*/*Result/02.Contig/*stat |head -n 1 > p0 
-    cat p0 p1 > 6CAMI.dataset.all.sample.contig.stat.txt 
-    rm -rf p0 p1 
